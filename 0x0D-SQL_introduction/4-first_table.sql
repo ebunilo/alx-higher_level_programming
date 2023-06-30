39d7c7a3e4d52abfd48f995c9ff75fb9a28033a6
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2023-06-30 10:26:48 +0100

    create table if not exists

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- Create a table called first_table in the current database of your MySQL server
+-- with description: id INT, name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
