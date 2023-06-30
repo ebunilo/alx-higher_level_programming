8d77a7d1c76720aa76388f0f29f948fb748200f8
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2023-06-30 22:49:59 +0100

    delete record

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,8 @@
+-- Remove all records with a score <= 5 in the table second_table
+-- The datatbase name will be passed as an argument of the mysql command
+DELETE 
+    * 
+FROM 
+    `second_table`
+WHERE
+    `score` <= 5;
