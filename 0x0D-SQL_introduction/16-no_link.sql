10e9481c5af8c5dd3e4fae8a295527b0b92752b0
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2023-07-01 00:41:38 +0100

    list records where field not NULL

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,11 @@
+-- List all records of the table second_table 
+-- Don't list rows without a name value
+-- Results should display the score and the name (in this order)
+-- Records should be listed by descending score
+SELECT
+    `score`, `name`
+FROM
+    `second_table`
+WHERE
+    `name` != ""
+ORDER BY `score` DESC;
