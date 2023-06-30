dcb215cac7cad388a5ccfc78b3e9f6d546d044ea
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2023-06-30 10:52:50 +0100

    match quote

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- Print the full description of the first_table from database hbtn_0c_0 in your MySQL server
 -- The database name will be passed as an argument of the mysql command
-SHOW CREATE TABLE `first_table';
+SHOW CREATE TABLE `first_table`;
