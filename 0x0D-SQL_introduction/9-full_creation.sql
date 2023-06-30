c73373ee60ace57960b38f64652eaec37de6c147
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2023-06-30 20:04:16 +0100

    insert records

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- Create a table second_table in the database and add multiple rows
+-- second_table description: id INT, name VARCHAR(256), score INT
+-- The database name will be passed as an argument to the mysql command
+CREATE TABLE IF NOT EXISTS `second_table` (id INT, name VARCHAR(256), score INT);
+INSERT INTO `second_table` (`id`, `name`, `score`)
+VALUES
+    (1, "John", 10), 
+    (2, "Alex", 3), 
+    (3, "Bob", 14), 
+    (4, "Goerge", 8);
