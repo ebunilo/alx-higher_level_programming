84dc9548bc3a7190b10bfa2dbee011a07af26297
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2023-06-30 23:11:14 +0100

    refactor statements

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,6 +1,4 @@
 -- Compute the score average of all records in the table second_table
 -- The database name will be passed as an argument of the mysql command
-SELECT 
-    AVG('score') AS `average`
-FROM
-    `second_table`;
+SELECT AVG('score') AS `average`
+FROM `second_table`;
