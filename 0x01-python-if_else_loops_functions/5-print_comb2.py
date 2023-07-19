e14d5c5cc1e7d300aa79053ede33ac6043145400
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2023-07-19 07:31:10 +0100

    update range

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,6 +3,6 @@
 Print numbers 0 to 99 separated by , followed by a space
 Numbers should be printed in ascending order, with two digits
 """
-for number in range(0, 98):
+for number in range(0, 99):
     print("{:02d}, ".format(number), end="")
-print("98\n")
+print("99\n")
