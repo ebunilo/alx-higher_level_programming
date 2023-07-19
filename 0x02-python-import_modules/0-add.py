c153f3cde6c0dc6e1a18693b578c6c1fb0bcedd2
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2023-07-19 23:36:26 +0100

    add function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """
-Import the function def add(a, b) from the file add0.py 
+Import the function def add(a, b) from the file add_0.py 
 and print the result of the addition 1 + 2 = 3
 """
-from add0.py import add
+from add_0 import add
 a = 1
 b = 2
 print("{} + {} = {}".format(a, b, add(a, b)))
-
