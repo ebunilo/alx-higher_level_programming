dd3575c9e3f26406ee8cf3809579b5948bc113f2
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2023-07-19 06:42:30 +0100

    use hex function

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 """Print all numbers from 0 to 98 in decimal and hexadecimal"""
 for i in range(0, 99):
-    print("{} = {:x}".format(i, i))
+    print("{} = {}".format(i, hex(i)))
