8514638dc5d20d0768ca445219f10224432126b9
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2023-07-20 00:46:09 +0100

    refactor code

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,11 +3,9 @@
 Import the function def add(a, b) from the file add_0.py 
 and print the result of the addition 1 + 2 = 3
 """
-from add_0 import add
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
-
 if __name__ == "__main__":
-    import sys
-    add(int(sys.argv[2]))
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
