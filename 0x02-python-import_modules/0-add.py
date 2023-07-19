62b83093353155b37e76fe2561e8c6f707f76961
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2023-07-20 00:53:00 +0100

    rename file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Import the function def add(a, b) from the file 
-and print the result of the addition 1 + 2 = 3
+Add two numbers
 """
 if __name__ == "__main__":
     from add_0 import add
