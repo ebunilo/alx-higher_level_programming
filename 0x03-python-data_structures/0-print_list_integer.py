d744c155f77a0d467c07cb23732831e924c979d8
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2023-07-20 22:54:31 +0100

    pycodestyle

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
+
 def print_list_integer(my_list=[]):
     """Print all integers in a list
 
     Args:
-        my_list (list, optional): the list of integers to print. Defaults to [].
+        my_list (list, optional): the list of integers to print.
+        Defaults to [].
 
     Returns:
         None
