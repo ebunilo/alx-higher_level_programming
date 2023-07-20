f0ca1571b6156f1c1e455ffcca179223a0edaab1
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2023-07-20 22:49:06 +0100

    print all integers in a list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    """Print all integers in a list
+
+    Args:
+        my_list (list, optional): the list of integers to print. Defaults to [].
+
+    Returns:
+        None
+
+    """
+    for number in my_list:
+        print("{}".format(number))
