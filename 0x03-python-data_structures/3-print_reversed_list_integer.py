ba5ac4e010e5dacfc82d805cad00e48c6e25736b
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2023-07-21 00:38:03 +0100

    sort integer list in reverse

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def print_reversed_list_integer(my_list=[]):
+    """Print all integers of a list, in reverse order.
+
+    Args:
+        my_list (list, optional): list of integers to print. Defaults to [].
+
+    Returns:
+        None
+    """
+    my_list.sort(reverse=True)
+    for i in range(len(my_list)):
+        print("{}".format(my_list[i]))
