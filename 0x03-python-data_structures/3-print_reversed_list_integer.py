afd10506e144ecc5a91f5ad9f219c62450c4650c
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2023-07-21 00:43:23 +0100

    account for empty list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -10,6 +10,7 @@ def print_reversed_list_integer(my_list=[]):
     Returns:
         None
     """
-    my_list.sort(reverse=True)
+    if my_list:
+        my_list.sort(reverse=True)
     for i in range(len(my_list)):
         print("{:d}".format(my_list[i]))
