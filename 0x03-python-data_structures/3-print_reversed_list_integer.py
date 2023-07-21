f31752e10f18c39bc0d901e684bf23befefc2e17
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2023-07-21 01:06:38 +0100

    use reverse()

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -10,7 +10,10 @@ def print_reversed_list_integer(my_list=[]):
     Returns:
         None
     """
-    if my_list is not None:
-        my_list.sort(reverse=True)
+    if my_list:
+        my_list.reverse()
         for i in range(len(my_list)):
             print("{:d}".format(my_list[i]))
+
+
+print_reversed_list_integer([3, 0, 1, 5])
