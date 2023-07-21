e5c1dbb9f025481aca3019b1e42245a9d554ecab
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2023-07-21 07:33:52 +0100

    replace element in a list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+def new_in_list(my_list, idx, element):
+    """Replace an element in a list without modifying the original list
+
+    Args:
+        my_list (any type): list to be modified
+        idx (_type_): index position of element to be modified
+        element (_type_): element to be modified
+
+    Returns:
+        new list
+        original list if idx is negative
+        original list if idx is out of range
+    """
+    if idx < 0:
+        return my_list
+    if idx > len(my_list) - 1:
+        return my_list
+
+    new_list = my_list[:]
+    new_list[idx] = element
+
+    return new_list
